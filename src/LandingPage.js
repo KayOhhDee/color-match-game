9bfaff8516453873783929954c14a70b931aede0
kod3180 <kod3180@gmail.com>
2019-07-19 00:12:55 +0000

    description added to the landing page

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -4,6 +4,7 @@ const LandingPage = ({handleStart}) => {
   return (
     <div className='landing'>
       <h1>ColorMatch</h1>
+      <p><i>Find the correct pairs of colours to win the game.</i></p>
       <button onClick={handleStart}>Start</button>
     </div>
   );
